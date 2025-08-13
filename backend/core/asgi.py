70e4c102e1e959e6ef366f6eca92bd28755f0f2b
kmax11 <122941218+kmax11@users.noreply.github.com>
2025-08-13 22:36:21 +0300

    Update asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -3,7 +3,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket_urlpatterns
+from news.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 

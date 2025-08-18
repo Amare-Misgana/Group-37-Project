b625b288bae119d87537b33b84ef4bb96325e9b8
Amare Misgana <codeamare@gmail.com>
2025-08-19 01:32:52 +0300

    Added new views and apis

--- backend/student/views.py
+++ backend/student/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from admin_staff.models import CustomUser
+from django.db.models import F
 
-# Create your views here.
+
+class StudentManagementView(APIView):
+    def get(self, request, student_id):
+        try:
+            student = CustomUser.objects.get(id=student_id, role="student")
+        except CustomUser.DoesNotExist:
+            return Response(
+                {"error": f"A student with '{student_id}' id doesn't exist"}
+            )
+
+        return Response(
+            {
+                "student": CustomUser.objects.filter(id=student_id).values(
+                    "username",
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                    "age",
+                    "gender",
+                    "dorm_number",
+                    "phone_number",
+                    "guardian_number",
+                    "field_of_study",
+                )
+            }
+        )
